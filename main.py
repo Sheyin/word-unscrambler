1cb351c981969647b05f5455a22c17010579ded9
Sheyin <Sheyin@users.noreply.github.com>
2020-03-11 22:10:41 -0500

    Updated index to be centered

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 
 from functions import invalidInput, formatInput, generateCombinations
 import word
+import secret
 from copy import copy
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = secret.uri
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
